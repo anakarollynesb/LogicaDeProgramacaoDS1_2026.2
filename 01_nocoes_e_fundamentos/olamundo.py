e21484dfa6298a3e241f67bbdb9db426998a7e91
saviocunhaa <saviocunha.01@hotmail.com>
2026-09-10 16:14:29 -0300

    feat: codigo idade e nome

--- 01_nocoes_e_fundamentos/olamundo.py
+++ 01_nocoes_e_fundamentos/olamundo.py
@@ -1,2 +1,11 @@
-print("Savio Cunha")
-print("Ola Mundo!")
+# receba um nome 
+nome = input("digite o seu nome")
+# receba dia de nascimento 
+dia = input("digite o dia de nascimento")
+# receba mes de nascimento
+mes = input("digite o mes de nascismento")
+# receba o ano de Nascimento
+ano = input ("digite o ano de nascimento")
+# mostre tudo 
+
+print(f"sue nome é {nome} voce nasceu dia {dia}/{mes}/{ano} ")
