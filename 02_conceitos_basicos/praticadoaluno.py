6c834bd2b71f5952c70dfd82114567e7d48955db
anakarollynesb <ana.borges73@aluno.ce.gov.br>
2026-09-14 14:26:19 -0300

    praticadoaluno.py

---
+++ 02_conceitos_basicos/praticadoaluno.py
@@ -0,0 +1,12 @@
+# TODO: Declare as variáveis abaixo e execute a célula para testar
+nome_produto = "teclado"
+quantidade_estoque = 24
+preco_unitario = 50.0
+disponivel_para_venda = True
+
+# TODO: Escreva os prints de inspeção aqui
+
+print(type(nome_produto))
+print(type(quantidade_estoque))
+print(type(preco_unitario))
+print(type(disponivel_para_venda))
