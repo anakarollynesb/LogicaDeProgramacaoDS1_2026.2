e132be59c14730a029506f674213c4775abf5c34
anakarollynesb <ana.borges73@aluno.ce.gov.br>
2026-09-22 16:40:22 -0300

    praticamaonamassa01/3

---
+++ 03_estruturas_de_decisao/praticamaonamassa02.py
@@ -0,0 +1,8 @@
+# TODO: Escreva o programa do par ou ímpar aqui 
+# Escreva a estrutura if / else
+
+numero = int(input("Digite um número inteiro: "))   
+if numero %2:
+    print ("O número digitado é ímpar.")
+else:
+    print("O número digitado é par")
---
+++ testeBinario.py
@@ -0,0 +1,9 @@
+import decimal
+
+
+numero = int(input("Digite um número: ")) 
+codigo = (input("digite o numero binario:"))
+decimal = int 
+binario = bin(numero)[2:]
+print("Em binário:", binario)
+print("Em decimal:", bin 2 decimal)
